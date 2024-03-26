40b180db6757cf3fccd61b7900ef9f4243625df5
Felix <felix.chung@felixchung.net>
2024-03-27 00:17:49 +0800

    init

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This repository allows you to quickly spin up a data management platform made wi
 ## Pre-requisite
 1. Prepare a machine running in Ubuntu with Internet access. (Mine was Ubuntu 20.04 with 8vcpu / 32g memory in Azure when I developed this platform). I recommend a new and clean machine to avoid any issue
 2. Copy the setup.sh file from this repository into your machine
-3. Run the setup.sh as below to install JDK, Docker, Kubernetes and Jenkins in the machine
+3. Run the setup.sh as below to Docker, Kubernetes and Helm in the machine
 ```bash
 ## Go to the directory containing the setup.sh
 chmod +x setup.sh
@@ -23,4 +23,4 @@ microk8s enable dashboard dns ingress
 ```bash
 sudo cat <path shown on the screen>
 ```
-6. Choose "Install suggested plugins"
+6. Choose "Install suggested plugins"; and then skip and continue as admin; click "Not now" for Instance Configuration
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo current user: $USER
+echo home path: $HOME
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install K8s
+sudo snap install microk8s --classic
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# Run the microk8s commands with the new group using sg
+sg microk8s -c "microk8s enable dashboard dns ingress"
+sg microk8s -c "microk8s start"
+sg microk8s -c "microk8s enable hostpath-storage"
+sg microk8s -c "microk8s config > $HOME/.kube/config"
+
+# Create the namespace if it doesn't exist
+kubectl get namespace datahub-prerequisites-ns &> /dev/null || kubectl create namespace datahub-prerequisites-ns
+kubectl get namespace denodo-ns &> /dev/null || kubectl create namespace denodo-ns
+kubectl get namespace jenkins-ns &> /dev/null || kubectl create namespace jenkins-ns
+kubectl get namespace mariadb-ns &> /dev/null || kubectl create namespace mariadb-ns
+kubectl get namespace minio-ns &> /dev/null || kubectl create namespace minio-ns
+kubectl get namespace zammad-ns &> /dev/null || kubectl create namespace zammad-ns
+kubectl get namespace postgresql-ns &> /dev/null || kubectl create namespace postgresql-ns
+
+helm install datahub-prerequisites ./demo/datahub-prerequisites -n datahub-prerequisites-ns
+helm install denodo ./demo/denodo -n denodo-ns
+helm install jenkins ./demo/jenkins -n jenkins-ns
+helm install mariadb ./demo/mariadb -n mariadb-ns
+helm install minio ./demo/minio -n minio-ns
+helm install zammad ./demo/zammad -n zammad-ns
+helm install postgresql ./demo/postgresql -n postgresql-ns
